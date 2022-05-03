350a48b24b75d5da632a86753ce317ed3221cf72
Brian Hoffman <brian.hoffman@lyrasis.org>
2022-05-03 18:29:11 -0400

    ensure JSONModel::HTTP.http_conn gets overidden even if it was set before plugin could load

--- jsonmodel_http_proxy.rb
+++ jsonmodel_http_proxy.rb
@@ -3,8 +3,10 @@ module JSONModel::HTTP
     alias_method :http_conn_orig, :http_conn
 
     def http_conn
-      puts "overidden jsonmodel http conn"
+      @http = nil unless @overidden_http
+      puts "ASPACE PROXY PLUGIN: overiding jsonmodel http conn" unless @http
       @http ||= Net::HTTP::Persistent.new 'jsonmodel_client', URI("http://localhost:#{$proxy_port}")
+      @overidden_http ||= true
       http_conn_orig
     end
   end
